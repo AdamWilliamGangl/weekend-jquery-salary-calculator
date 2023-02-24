94f7161fc0f873195ef9bcb0d3efbf889b7e1ca0
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-24 15:42:35 -0600

    added form validationb, reset input fields, and created basic new employee push function.

--- script.js
+++ script.js
@@ -11,6 +11,44 @@ function readyNow() {
 
 //1a. an input form that collects first name, last name, ID number, job title,
 // annual salary.
+function addNewEmployee() {
+    //Created new varaibles to take in the value of the input fields
+    const firstName = $('#firstname').val();
+    const lastName = $('#lastname').val();
+    const id = $('#id').val();
+    const title = $('#title').val();
+    const annualSalary = $('#annualSalary').val();
+
+    //form validation part 1, ensure every field is filled.
+    if (firstName && lastName && id && title && annualSalary) {
+        let addNewEmployeeObject = {
+            employeeFirstName = firstName,
+            employeeLastName = lastName,
+            employeeID = id,
+            employeeTitle = title,
+            employeeAnnualSalary = annualSalary
+        };
+        employeeDatabase.push(addNewEmployeeObject);
+        render()
+    }
+    //form validation part 2, if a field is left blank- give an alert.
+    else {
+        alert('You have left a field blank!')
+    }
+}
+//function to render items to the DOM
+function render(){
+
+}
+
+function resetInputFields(){
+    const firstName = $('#firstname').val("");
+    const lastName = $('#lastname').val("");
+    const id = $('#id').val("");
+    const title = $('#title').val("");
+    const annualSalary = $('#annualSalary').val("");
+}
+
 
 //1b. A submit button to collect and store the form information.
 
