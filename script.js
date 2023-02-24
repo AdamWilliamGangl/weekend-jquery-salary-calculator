ea65b7926ef1f07cc17324d4b3e4c754aca0401a
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-24 15:26:48 -0600

    add javascript steps based on assignment.

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 console.log('Batman!')
 
+let employeeDatabase = [];
+let totalCosts = 0;
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log("DOM is loaded!");
+}
+
+//1a. an input form that collects first name, last name, ID number, job title,
+// annual salary.
+
+//1b. A submit button to collect and store the form information.
+
+//1c. Append the stored information to the DOM.
+
+//1d. Clear the input fields.
+
+
+//2a. Calculate monthly costs using stored data.
+
+//2b. Append that information to the DOM.
+
+//2c. If monthly costs exceed $20,000, add a red background color to total monthly cost.
+
+
+//3a. Create a delete button that removes an employee from the DOM.
+
+//3b. Remove the employees salart from the reported total.
+
