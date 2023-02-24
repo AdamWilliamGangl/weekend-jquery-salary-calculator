c876de9a8c768017456e5c31141ec24e7e1efdc3
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-24 16:42:04 -0600

    added multiple employees functionality, append the total monthly cost.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('Batman!')
 
 let employeeDatabase = [];
-let totalCosts = 0;
+let employeeCost = [];
 
 $(document).ready(readyNow);
 
@@ -13,7 +13,7 @@ function readyNow() {
 //1a. an input form that collects first name, last name, ID number, job title,
 // annual salary.
 function addNewEmployee() {
-    console.log('Hello!')
+    console.log('Here are the employees in the database', employeeDatabase)
     //Created new varaibles to take in the value of the input fields
     const firstName = $('#firstName').val();
     const lastName = $('#lastName').val();
@@ -31,8 +31,10 @@ function addNewEmployee() {
             employeeAnnualSalary: annualSalary
         };
         employeeDatabase.push(addNewEmployeeObject);
-        render();
+
         resetInputFields();
+        calculateMonthlyCost();
+        render();
     }
     //form validation part 2, if a field is left blank- give an alert.
     else {
@@ -40,43 +42,50 @@ function addNewEmployee() {
     }
 }
 //function to render items to the DOM
-function render(){
-    for (let employee of employeeDatabase){
+function render() {
+    $('.employeeTable').empty();
+    for (let employee of employeeDatabase) {
         $('.employeeTable').append(`
+        <tr>
         <td>${employee.employeeFirstName}</td>
         <td>${employee.employeeLastName}</td>
         <td>${employee.employeeID}</td>
         <td>${employee.employeeTitle}</td>
         <td>${employee.employeeAnnualSalary}</td>
-        `)
-    }
+        </tr>
+        `);
+    };
+    $('.monthlyCostContainer').html(`
+        <p>$${employeeCost}</p>
+        `); //need to append the sum of all employee salaries here.
+
 
 }
 
-function resetInputFields(){
-    const firstName = $('#firstname').val("");
-    const lastName = $('#lastname').val("");
+function resetInputFields() {
+    const firstName = $('#firstName').val("");
+    const lastName = $('#lastName').val("");
     const id = $('#id').val("");
     const title = $('#title').val("");
     const annualSalary = $('#annualSalary').val("");
 }
 
+function calculateMonthlyCost() {
 
-//1b. A submit button to collect and store the form information.
+    let employeeCostSum = 0;
+    employeeCost = [];
+    for (let cost of employeeDatabase) {
+        employeeCostSum += cost.employeeAnnualSalary; //This needs to not concatenate.
+    }
+    employeeCost = employeeCostSum;
+    console.log('employee Cost', employeeCost);
+}
 
-//1c. Append the stored information to the DOM.
-
-//1d. Clear the input fields.
-
-
-//2a. Calculate monthly costs using stored data.
-
-//2b. Append that information to the DOM.
 
 //2c. If monthly costs exceed $20,000, add a red background color to total monthly cost.
 
 
 //3a. Create a delete button that removes an employee from the DOM.
 
-//3b. Remove the employees salart from the reported total.
+//3b. Remove the employees salary from the reported total.
 
